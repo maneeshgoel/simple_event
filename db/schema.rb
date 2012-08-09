3ecaec431f9890c3d4d7519ae4b062a1cbf6efb8
Maneesh Goel <maneeshgoel@gmail.com>
2012-08-08 18:35:22 -0700

    change migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120805063112) do
+ActiveRecord::Schema.define(:version => 20120809013314) do
 
   create_table "attendees", :force => true do |t|
     t.integer  "user_id"
@@ -25,9 +25,9 @@ ActiveRecord::Schema.define(:version => 20120805063112) do
     t.string   "details"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.integer  "user_id"
     t.string   "name"
     t.datetime "datetime"
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
